397a5fbdb62b98710bffd4c3269c6432ba2883fb
MaxKievskiy <maxkievskiy90@gmail.com>
2021-01-09 16:15:27 +0300

    testing

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,9 +4,12 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
+            <div>
             <img
                 src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Coat_of_arms_of_Ramat_Gan.svg/1200px-Coat_of_arms_of_Ramat_Gan.svg.png'/>
+            </div>
         </header>
+
     )
 }
 export default Header;
