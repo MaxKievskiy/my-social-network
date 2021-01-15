a737525c65358ce6ba586c6af1e071cfca138d2e
MaxKievskiy <maxkievskiy90@gmail.com>
2021-01-15 18:44:30 +0300

    25,26,27

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,55 +1,41 @@
 import React from 'react';
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogsItem";
+import Message from "./DialogItem/Message";
 
-let dialogData = [
-    {name: 'Julia', id: 1},
-    {name: 'Avraam', id: 2},
-    {name: 'Daniel', id: 3},
-    {name: 'Katrin', id: 4},
-    {name: 'Elena', id: 5},
-    {name: 'Rivkaa', id: 6}
-
-]
-let messageData = [
-    {message: 'Hello!!!'},
-    {message: 'How are you?'},
-    {message: 'Bye'},
-    {message: 'Yo',},
-    {message: 'Yes',}
-
-]
-
-
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
-    return <div className={s.dialog + " " + s.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-}
-
-const Message = (props) => {
-    return (
-        <div className={s.message}> {props.message}</div>
-    )
-}
 const Dialogs = (props) => {
+    let dialogData = [
+        {name: 'Julia', id: 1},
+        {name: 'Avraam', id: 2},
+        {name: 'Daniel', id: 3},
+        {name: 'Katrin', id: 4},
+        {name: 'Elena', id: 5},
+        {name: 'Rivkaa', id: 6}
+
+    ]
+
+    let messageData = [
+        {message: 'Hello!!!'},
+        {message: 'How are you?'},
+        {message: 'Bye'},
+        {message: 'Yo',},
+        {message: 'Yes',}
+
+    ]
+
+    let dialogElement = dialogData.map((dialog) => <DialogItem name={dialog.name} id={dialog.id}/>)
+
+    let messageElement = messageData.map((message) => <Message message={message.message}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItem}>
-                <DialogItem name={dialogData[0].name} id={dialogData[0].id}/>
-                <DialogItem name={dialogData[1].name} id={dialogData[1].id}/>
-                <DialogItem name={dialogData[2].name} id={dialogData[2].id}/>
-                <DialogItem name={dialogData[3].name} id={dialogData[3].id}/>
-                <DialogItem name={dialogData[4].name} id={dialogData[4].id}/>
-                <DialogItem name={dialogData[5].name} id={dialogData[5].id}/>
+                {dialogElement}
+                {/*<DialogItem name={dialogData[0].name} id={dialogData[0].id}/>*/}
             </div>
             <div className={s.messages}>
-                <Message message={messageData[0].message}/>
-                <Message message={messageData[1].message}/>
-                <Message message={messageData[2].message}/>
-                <Message message={messageData[3].message}/>
-                <Message message={messageData[4].message}/>
+                {messageElement}
+                {/*<Message message={messageData[0].message}/>*/}
             </div>
         </div>
     )
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
-let postData = [
-    {post: 'Hi, how are you?', likeData: 34},
-    {post: 'I am fine', likeData: 10}
-]
-
-
 const MyPosts = () => {
+
+    let postData = [
+        {post: 'Hi, how are you?', likeData: 34},
+        {post: 'I am fine', likeData: 10}
+    ]
+
+    let postElements = postData.map((post => <Post message={post.post} likesCount={post.likeData}/>))
+
     return (
         <div className={s.myPostsBlock}>
             <h3>My posts</h3>
@@ -23,11 +25,10 @@ const MyPosts = () => {
 
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].post} likesCount={postData[0].likeData}/>
-                <Post message={postData[1].post} likesCount={postData[1].likeData}/>
+                {postElements}
+                {/*<Post message={postData[0].post} likesCount={postData[0].likeData}/>*/}
             </div>
         </div>
     )
 }
-
 export default MyPosts;
