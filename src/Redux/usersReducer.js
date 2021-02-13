ac0b2a901c7c9e152a6277f65319d4365f403dde
MaxKievskiy <maxkievskiy90@gmail.com>
2021-02-13 20:41:44 +0300

    update 55

--- src/Redux/usersReducer.js
+++ src/Redux/usersReducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const TOTAL_USERS_COUNT = 'TOTAL_USERS_COUNT';
 
 let initialState = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -31,9 +36,17 @@ const usersReducer = (state = initialState, action) => {
             }
         case SET_USERS:
             return {
-                ...state, users: [...state.users, ...action.users]
+                ...state, users: [...action.users]
             }
-            default:
+        case SET_CURRENT_PAGE:
+            return {
+                ...state, currentPage: action.currentPage
+            }
+        case TOTAL_USERS_COUNT:
+            return {
+                ...state, totalUsersCount: action.count
+            }
+        default:
             return state;
     }
 }
@@ -42,5 +55,10 @@ export const followAC = (userId) => ({type: FOLLOW, userId});
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
 
 export const setUsersAC = (users) => ({type: SET_USERS, users});
+
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+
+export const setTotalUsersCountAC = (count) => ({type: TOTAL_USERS_COUNT, count});
+
 
 export default usersReducer;
