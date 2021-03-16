eccd3e58a999069df76be81196e13f69a527b35c
MaxKievskiy <maxkievskiy90@gmail.com>
2021-03-16 22:13:13 +0300

    update 75/1

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+const LoginForm = (props) => {
+    return <form>
+        <div>
+            <input placeholder={"Login"}/>
+        </div>
+        <div>
+            <input placeholder={"Password"}/>
+        </div>
+        <div>
+            <input type="checkbox"/>remember me
+        </div>
+        <div>
+            <button>Sign in</button>
+        </div>
+    </form>
+}
+
 const Login = (props) => {
     return <div>
         <h1>Login</h1>
+        <LoginForm/>
     </div>
 }
 
